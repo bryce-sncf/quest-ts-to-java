b9978e0e7b9f9eac0daca2a575dbcb04893aec04
bryce-sncf <bryce.marcol@sncf.fr>
2023-07-04 16:21:21 +0200

    initial

---
+++ Address.java
@@ -0,0 +1,38 @@
+public class Address {
+    private int number;
+    private String street;
+    private String city;
+    private String country;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
